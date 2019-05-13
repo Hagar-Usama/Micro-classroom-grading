f43fd863b843c96a16622ffb25ea10be612653aa
Hagar <hagar_usama@outlook.com>
2019-05-14 00:18:56 +0200

    change color unconditionally

--- Bonus/simple_paint.asm
+++ Bonus/simple_paint.asm
@@ -5,31 +5,39 @@ code segment
 main proc far
  
  
- 
+; preparing for graphics mode 
 mov al, 13h
 mov ah, 0   ; set graphics video mode.
 int 10h   
 
+;making initial color is green
 mov al, 2 
 mov color ,al
 
+;start of program :
 Next:
-
 mov ax, 3   ;get cursor positon in cx,dx
 int 33h
 
-;call hide_cursor
+;useless for some reason
+call show_cursor
 
-    
-cmp bx , 2
-je  change  
-
+; if left_click , then draw 
 cmp bx , 1
 jne Next
-;if left click
+
+call putpix ;call procedure
+
+ call change_color
+;if right_click, change_color
+;cmp bx , 2
+;jne Next
+
+;call change_color
+jmp Next
 
   
-call putpix ;call procedure 
+ 
  
 jmp Next
 
@@ -48,12 +56,21 @@ main endp
 ;procedure to print
 putpix proc   
 mov al, color  ;color of pixel  
+;mov al , 2
 mov ah, 0ch    
 shr cx,1    ; cx will get double so we divide it by two
 int 10h     ; set pixel.
 ret
 putpix endp 
 
+;*****************************************
+change_color proc 
+    mov al , color
+    inc al
+    mov color , al
+    
+ret
+change_color endp
 
 ;******************************************
 show_cursor proc
