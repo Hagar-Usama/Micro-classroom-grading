4b57072cedf0f65e442dd5193dd76adb5be2a86f
Hagar <hagar_usama@outlook.com>
2019-05-14 02:06:52 +0200

    minor change

--- paints.asm
+++ paints.asm
@@ -1,7 +1,24 @@
-.data
-color db 2
+; multi-segment executable file template.
+
+data segment
+    ; add your data here!
+    pkey db "press any key...$"
+    color db 2
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
 
 code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+    ;**************************************************
+    ;##################################################
+
 main proc far
                      
  
@@ -152,3 +169,20 @@ CLRSCR ENDP
 ;********************
 
 code ends 
+    
+
+    ;##################################################
+    ;**************************************************        
+    lea dx, pkey
+    mov ah, 9
+    int 21h        ; output string at ds:dx
+    
+    ; wait for any key....    
+    mov ah, 1
+    int 21h
+    
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.
